7775295e634a98d068ad22d5be91c214328aca59
Aiden Mendez <aidenmendez@gmail.com>
2020-10-07 14:06:58 -0600

    Update minor wording issues in test file

--- test/restaurant_test.rb
+++ test/restaurant_test.rb
@@ -60,7 +60,7 @@ class RestaurantTest < Minitest::Test
     assert_equal false, restaurant2.open_for_lunch?
   end
 
-  def test_capitalizing_menu_dish_names
+  def test_capitalizing_with_menu_dish_names
     restaurant = Restaurant.new('16:00', 'Il Poggio')
     restaurant.add_dish('Burrata')
     restaurant.add_dish('Pizzetta')
@@ -82,7 +82,7 @@ class RestaurantTest < Minitest::Test
     assert_equal "Fuel Cafe will be closing at 12:00PM", restaurant.announce_closing_time(6)
   end
 
-  def test_announce_closing_time_noon
+  def test_announce_closing_time_midnight
     restaurant = Restaurant.new('16:00', 'Il Posto')
 
     assert_equal "Il Posto will be closing at 12:00AM", restaurant.announce_closing_time(8)
